ed3fa059b47d665d63fe8d7c12d5b123191b32ef
CristianGaudino <gaudino.cristian@gmail.com>
2020-04-23 11:19:19 +0100

    Think I found the bug

--- youask/register.py
+++ youask/register.py
@@ -34,22 +34,15 @@ if len(form_data) !=0:
     display_name=escape(form_data.getfirst('display_name', '').strip())
     password1=escape(form_data.getfirst('password1', '').strip())
     password2=escape(form_data.getfirst('password2', '').strip())
-    username_msg="<h1>Throw me a bone</h1>" if username == "" else username
-    email_msg=email
-    display_msg=display_name
-    password_msg=password
     if not username or not  email or not display_name or not password1 or not password2:
         error_msg='<p class="error">All Fields Must Be Filled</p>'
     else:
         #All return 'clear' if they pass the stipulations
         user_result, email_result, display_result, pass_result = registrationValidation(username, email, display_name, password)
-        error_msg="<h1>Throw me a bone</h1>" if user_result == "" else user_result
-        email_msg=email_result
-        display_msg=display_result
-        password_msg=pass_result
 
         #if all clear then do try except
         #else if statement for each result and let msg equal result
+
         if user_result == 'clear' and email_result == 'clear' and display_result == 'clear' and pass_result == 'clear' and password1 == password2:
             try:
                 connection, cursor=dbConnect()
@@ -65,7 +58,7 @@ if len(form_data) !=0:
                     cookie=SimpleCookie()
                     sid = sha256(repr(time()).encode()).hexdigest()
                     cookie['sid'] = sid
-                    session_store = open('session_store/sess_'+ sid, writeback=True)
+                    session_store = open('sess_'+ sid, writeback=True)
                     session_store['authenticated']=True
                     session_store['username']=username
                     session_store['email']=email
@@ -100,7 +93,10 @@ if len(form_data) !=0:
             elif pass_result!='clear':
                 password_msg=pass_result
                 print(password_msg)
-    #error_msg='<p class="error">Server Error Occurred</p>' if server_error==True else ""
+
+    error_msg='<p class="error">Server Error Occurred</p>' if server_error==True else ""
+
+
 
 
 print('Content-Type: text/html')
