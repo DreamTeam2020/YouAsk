c6df67c1274e416d4d4f55ed084c54772faf01e5
CristianGaudino <gaudino.cristian@gmail.com>
2020-04-30 17:20:05 +0100

    Updated question submission to match the changes to MVC style

--- youask/controller/ctrl_submit.py
+++ youask/controller/ctrl_submit.py
@@ -1,29 +1,45 @@
 from cgi import escape
 from model.model_functions import *
+from controller.html_functions import *
+from controller.ctrl_cache import *
+from cgi import FieldStorage
 
-def controllerSubmission(form_data, username):
-    # Controller for the submit view, take in the form_data, verify it and then submit the question if verified
-    server_error=False
-    input_error=False
-    submitted=False
+def controllerSubmission():
+    # Controller for the submit view, verify user input and then submit the question if verified
 
+    url = "submit.py"
+    question = ""
+    description = ""
+    result = loginToAccess()
     error_msg = "<p> </p>"
 
-    question= escape(form_data.getfirst('question', '').strip())
-    description = escape(form_data.getfirst('description', '').strip())
+    # Check if user is logged in
+    # If logged in print form then do len form data
+    username=verifyLoggedIn(False)   # Returns username if logged in, else UNVERIFIED
 
-    if not question:
-        error_msg='<p class="error">Question Field Must Be Filled</p>'  # If no question is entered
-    else:
-        # Filter out profanity on description, block questions that include profanity
-        if len(question) < 5:   #Remove this later for proper verification
-            input_error=True
-        else:
-            # If input has been verified then insert the user's question in the database
-            submission_result = submitQuestion(username, question, description)
-            if submission_result=="SERVER_ERROR":
-                server_error=True
+    if username != 'UNVERIFIED':  # If the user is logged in, print the question submission form
+        result = generateQuestionForm(url, question, description, error_msg)
+
+        form_data=FieldStorage()
+
+        if len(form_data)!=0:
+            question= escape(form_data.getfirst('question', '').strip())
+            description = escape(form_data.getfirst('description', '').strip())
+
+            if not question:
+                error_msg='<p class="error">Question Field Must Be Filled</p>'  # If no question is entered
             else:
-                submitted=True
+                # Filter out profanity on description, block questions that include profanity
+                if len(question) < 5:    # Remove this later for proper verification
+                    error_msg = '<p class="error">Invalid question, please <em>Do Not</em> include profanity ' \
+                                'within the question. Profanity within the description will be filtered out</p>'
+                else:
+                    # If input has been verified then insert the user's question in the database
+                    submission_result = submitQuestion(username, question, description)
+                    if submission_result=="SERVER_ERROR":
+                        error_msg = '<p class="error">Server Error Occurred</p>'
+                    else:
+                        error_msg = '<p class="error">Question Has Been Submitted</p>'
 
-    return submitted, server_error, input_error, error_msg
+            result = generateQuestionForm(url, question, description, error_msg)
+    return result
--- youask/submit.py
+++ youask/submit.py
@@ -6,9 +6,6 @@ enable()
 from controller.html_functions import *
 from controller.ctrl_cache import *
 from controller.ctrl_submit import *
-from http.cookies import SimpleCookie
-from os import environ
-from shelve import open
 from cgi import FieldStorage
 
 # Can't submit unless logged in, user enters Question and optional description (Markdown) into form
@@ -16,32 +13,8 @@ from cgi import FieldStorage
 page_name="submit"
 url="submit.py"
 
-question=""
-description=""
-result = loginToAccess()
-error_msg="<p> </p>"
+result=controllerSubmission()
 
-#Check if user is logged in
-# If logged in print form then do len form data
-verify_login=verifyLoggedIn(False)   # Returns username if logged in, else false
-
-if verify_login!='UNVERIFIED':  # If the user is logged in, print the question submission form
-    result=generateQuestionForm(url, question, description, error_msg)
-
-    form_data = FieldStorage()
-    if len(form_data)!=0:
-        submitted, server_error, input_error, error_msg=controllerSubmission(form_data, verify_login)
-
-        if submitted==True:
-            error_msg = '<p class="error">Question Has Been Submitted</p>'
-            #Provide link to the question page
-        elif server_error==True:
-            error_msg = '<p class="error">Server Error Occurred</p>'
-        elif input_error==True:
-            error_msg = '<p class="error">Invalid question, please <em>Do Not</em> include profanity within the question. ' \
-                        'Profanity within the description will be filtered out</p>'
-
-        result = generateQuestionForm(url, question, description, error_msg)
 print('Content-Type: text/html')
 print()
 
