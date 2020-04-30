1eb7509a84d4a84f40c8de559328d389810e9ac2
YC <isyangc2@gmail.com>
2020-04-30 12:28:47 +0100

    Using different <p> to display different question
    first commit, it may need to improve

--- youask/questions.py
+++ youask/questions.py
@@ -2,11 +2,17 @@
 
 from cgitb import enable
 
+from model.model_functions import getQuestion
+
 enable()
 
 from controller.html_functions import *
 
+list1 = []
 page_name = "questions"
+result = getQuestion()
+for x in result:
+    list1.append(result[x])
 
 print('Content-Type: text/html')
 print()
@@ -17,9 +23,19 @@ print("""
         <header>    <!-- A header section displayed at the top of the page--->
 
         </header>
+        <script>
+        function Add(one) {
+        for(var i=0; i<one.length;i++){
+    var para = document.createElement("p");
+    para.innerHTML=one[i];
+    document.body.appendChild(para);}
+    }
+        
+        </script>
 
         <main>      <!-- The main part of the website --->
             <h1>test page</h1>
+            Add(list1)
         </main>
 
         <aside>     <!-- A small aside that contains information not related to the main --->
@@ -28,4 +44,4 @@ print("""
 
         %s
         %s
-    """ % (pageStart("Questions", page_name), generateNav(page_name), pageEnd()))
+    """ % (pageStart("Questions", page_name),  generateNav(page_name), pageEnd()))
