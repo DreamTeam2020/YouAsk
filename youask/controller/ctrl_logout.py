c3e6ca3e11dd71c92de262a80229c3617ab09e5d
CristianGaudino <gaudino.cristian@gmail.com>
2020-05-01 12:35:16 +0100

    Fixed small bug with logout causing ISE

--- youask/controller/ctrl_logout.py
+++ youask/controller/ctrl_logout.py
@@ -3,7 +3,13 @@ from http.cookies import SimpleCookie
 from os import environ
 
 def controllerLogout():
-    result='<p class="Error">You are not logged in</p>'
+    result="""  <section>
+                    <p class="Error">You are not logged in</p>
+                    <ul>
+                        <li><a href="register.py">Register</a></li>
+                        <li><a href="login.py">Log In</a></li>
+                    </ul>
+                </section"""
     verify_logged=verifyLoggedIn(False)
 
     if verify_logged!='UNVERIFIED':
@@ -13,11 +19,13 @@ def controllerLogout():
         http_cookie_header = environ.get('HTTP_COOKIE')
         cookie.load(http_cookie_header)
         sid = cookie['sid'].value
-        session_store = open('sess_' + sid, writeback=True)
+        session_store = open('session_store/sess_' + sid, writeback=True)
         session_store['authenticated'] = False
         session_store.close()
         result="""
                 <p>You are now logged out.</p>
                 <p><a href="login.py">Log Back In Here</a></p>"""
+
+
 
     return result
