f2d4bfda7c660885384826c36dd68da5483d815b
YC <isyangc2@gmail.com>
2020-05-06 14:55:18 +0100

    Fix a parameter bug in controller search

--- youask/controller/ctrl_search.py
+++ youask/controller/ctrl_search.py
@@ -1,13 +1,12 @@
-
 from model.model_functions import dbConnect, dbClose
 
 
 def searchkeyword(keyword):
     connection, cursor = dbConnect()
 
-    cursor.execute("SELECT question FROM ask_questions WHERE name REGEXP  %s", keyword)
+    cursor.execute("SELECT question FROM ask_questions WHERE question  REGEXP  %s", keyword)
     connection.commit()
     fetch = cursor.fetchall()
     dbClose(connection, cursor)
-    return fetch[0]
+    return fetch
 
