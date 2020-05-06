9c4bd34ceed8b04d083391c6dde781916021c97f
YC <isyangc2@gmail.com>
2020-05-06 14:47:09 +0100

    Fix a small bug

--- youask/controller/ctrl_search.py
+++ youask/controller/ctrl_search.py
@@ -1,11 +1,13 @@
+
 from model.model_functions import dbConnect, dbClose
 
 
 def searchkeyword(keyword):
     connection, cursor = dbConnect()
 
-    cursor.execute("SELECT question FROM ask_questions WHERE question  REGEXP  %s", keywo$
+    cursor.execute("SELECT question FROM ask_questions WHERE name REGEXP  %s", keyword)
     connection.commit()
     fetch = cursor.fetchall()
     dbClose(connection, cursor)
-    return fetch
+    return fetch[0]
+
