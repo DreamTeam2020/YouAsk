b7ce2a0e36dcfa1e51279d7ac65757882d044706
YC <isyangc2@gmail.com>
2020-05-06 16:19:03 +0100

    Refactor the code
    To meet our code style

--- youask/controller/ctrl_search.py
+++ youask/controller/ctrl_search.py
@@ -1,6 +1,6 @@
 from cgi import FieldStorage, escape
 
-from model.model_functions import dbConnect, dbClose
+from model.model_functions import dbConnect, dbClose, questionsearch
 
 
 def searchkeyword():
@@ -8,14 +8,7 @@ def searchkeyword():
     form_data = escape(form_data.getfirst('search', '').strip())
     fetch=" "
     if len(form_data) != 0:
-
-        connection, cursor = dbConnect()
-
-        cursor.execute("SELECT question FROM ask_questions WHERE question  REGEXP  %s", form_data)
-        connection.commit()
-        fetch = cursor.fetchall()
-        dbClose(connection, cursor)
-        return fetch
+        fetch=questionsearch(form_data)
     return fetch
 
 
