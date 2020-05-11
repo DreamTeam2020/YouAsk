067b2676d0f4a950d0c6d82e25b66115f98a99d1
YC-git <MY_NAME@example.com>
2020-05-11 16:14:24 +0100

    add functionality share to facebook in questions.py page

--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -4,9 +4,9 @@ from model.model_functions import getQuestion
 def controllerQuestions():
     result = getQuestion()
     length = len(result)
-    questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp '
+    questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp  '
     for x in result:
-        questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s</section>' % (
-            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"])
+        questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s</section> <a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" style="margin-right:19px;">Share to facebook</a> ' % (
+            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"],x)
 
     return questions
