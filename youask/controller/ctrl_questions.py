7609eb51b954a5c7b0e123678d98d82ef8e60262
YC-git <MY_NAME@example.com>
2020-05-12 16:16:50 +0100

    sort questions bug fix

--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -1,11 +1,12 @@
 from model.model_functions import getQuestion
+import datetime
 
 
 
 
 def controllerQuestions():
     result = getQuestion()
-    insertionSort(result)
+    result=insertionSort(result)
     questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
     for x in result:
         SharetoFb = '<h5><a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> </h5>' % x
@@ -20,22 +21,34 @@ def controllerQuestions():
 
 def dateconvert(s):
    news=""
-   for x in range(len(s)):
-       if 47 < ord(s[x]) < 58:
+   for x in range(19):
+       if 48 <= ord(s[x]) <= 57:
            news+=s[x]
-           print(ord(s[x]))
-       else: print(ord(s[x]))
 
-   return int(news)
+
+
+   return (news)
+
+
+def conventdate(arr):
+    for i in range(len(arr)):
+        print(i+1)
+        arr[i]["submission_date"]=arr[i]["submission_date"].strftime("%y")+ arr[i]["submission_date"].strftime("%m")+arr[i]["submission_date"].strftime("%d")+arr[i]["submission_date"].strftime("%H")+ arr[i]["submission_date"].strftime("%M")+arr[i]["submission_date"].strftime("%S")
+        arr[i]["submission_date"]=int(arr[i]["submission_date"])
+    return arr
+
 
 
 def insertionSort(arr):
     for i in range(1, len(arr)):
-
-        key = dateconvert(arr[i]["submission_date"])
-
+        key =arr[i]["submission_date"]
         j = i - 1
-        while j >= 0 and key < dateconvert(arr[j]["submission_date"]):
+        while j >= 0 and key < arr[j]["submission_date"]:
             arr[j + 1] = arr[j]
             j -= 1
+
         arr[j + 1]["submission_date"] = key
+    return arr
+
+
+
