4ef33b39d20939a6f3a4fd298069bf2f8c9487bb
YC-git <MY_NAME@example.com>
2020-05-12 13:39:23 +0100

    sort questions

--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -1,9 +1,11 @@
 from model.model_functions import getQuestion
 
 
+
+
 def controllerQuestions():
     result = getQuestion()
-    length = len(result)
+    insertionSort(result)
     questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
     for x in result:
         SharetoFb = '<h5><a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> </h5>' % x
@@ -16,3 +18,24 @@ def controllerQuestions():
 
 
 
+def dateconvert(s):
+   news=""
+   for x in range(len(s)):
+       if 47 < ord(s[x]) < 58:
+           news+=s[x]
+           print(ord(s[x]))
+       else: print(ord(s[x]))
+
+   return int(news)
+
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+
+        key = dateconvert(arr[i]["submission_date"])
+
+        j = i - 1
+        while j >= 0 and key < dateconvert(arr[j]["submission_date"]):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1]["submission_date"] = key
