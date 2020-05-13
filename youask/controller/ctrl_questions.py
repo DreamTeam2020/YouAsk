851de43947a6d70e3cb01361c402c02fdaf94758
YC-git <MY_NAME@example.com>
2020-05-13 11:58:11 +0100

    move share link into section

--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -1,26 +1,5 @@
 from model.model_functions import getQuestion
 
-
-
-
-
-def controllerQuestions():
-    result = getQuestion()
-    result=conventdate(result)
-    result=insertionSort(result)
-    questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
-    for x in result:
-        SharetoFb = '<h5><a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> </h5>' % x
-        SharetoTw = '<h5><a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja">Share to Twitter</a></h5>' % x
-        questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s </section> %s %s' % (
-            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"],SharetoFb,SharetoTw)
-
-    return questions
-
-
-
-
-
 def conventdate(arr):
     for i in range(len(arr)):
         arr[i]["submission_date"]=arr[i]["submission_date"].strftime("%y")+ arr[i]["submission_date"].strftime("%m")+arr[i]["submission_date"].strftime("%d")+arr[i]["submission_date"].strftime("%H")+ arr[i]["submission_date"].strftime("%M")+arr[i]["submission_date"].strftime("%S")
@@ -44,4 +23,29 @@ def insertionSort(arr):
 
 
 
+def controllerQuestions():
+    result = getQuestion()
+    result=conventdate(result)
+    result=insertionSort(result)
+    questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
+    for x in result:
+        SharetoFb = '<p><a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> </p>' % x
+        SharetoTw = '<p><a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja">Share to Twitter</a></p>' % x
+        questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s %s %s</section> ' % (
+            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"],SharetoFb,SharetoTw)
 
+    return questions
+
+
+
+
+
+
+
+
+'''
+result = getQuestion()
+result=conventdate(result)
+result=insertionSort(result)
+print(result)
+'''
