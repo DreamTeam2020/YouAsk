cedfe20792a5b47f40c6634af7f44b5d0a509118
YC-git <MY_NAME@example.com>
2020-05-13 12:13:46 +0100

    move share link into section(fixed a bug)

--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -29,8 +29,8 @@ def controllerQuestions():
     result=insertionSort(result)
     questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
     for x in result:
-        SharetoFb = '<p><a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> </p>' % x
-        SharetoTw = '<p><a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja">Share to Twitter</a></p>' % x
+        SharetoFb = '&nbsp &nbsp &nbsp <a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;">Share to facebook</a> ' % x
+        SharetoTw = '&nbsp &nbsp &nbsp <a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja">Share to Twitter</a><p></p>' % x
         questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s %s %s</section> ' % (
             x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"],SharetoFb,SharetoTw)
 
