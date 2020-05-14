d959a3f984e15eb24572981c89e7c13c7c560aba
CristianGaudino <gaudino.cristian@gmail.com>
2020-05-14 16:39:58 +0100

    Added Yang's social media sharing to question specific pages

--- youask/controller/ctrl_question_page.py
+++ youask/controller/ctrl_question_page.py
@@ -37,17 +37,18 @@ def generateQuestion(question):
 
         fields_of_study=fields_of_study[:-3]    # Remove the last 3 characters of the string
 
-    facebook_src = os.getcwd() + "images/Facebook.png"
-    twitter_src = os.getcwd() + "images/Twitter.png"
-    SharetoFb = '<a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;"> <img src=%s style="border:none 0;" alt="Share to Facebook" /></a> ' % (question_id, facebook_src)
-    SharetoTw = '<a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja"><img src=%s style="border:none 0;" alt="Share to Twitter" /></a></p>' % (question_id, twitter_src)
+    facebook_src = os.getcwd() + "../images/Facebook.png"
+    twitter_src = os.getcwd() + "../images/Twitter.png"
+    share_to_fb = '<a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;"> <img src=%s style="border:none 0;" alt="Share to Facebook" /></a> ' % (question_id, facebook_src)
+    share_to_tw = '<a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja"><img src=%s style="border:none 0;" alt="Share to Twitter" /></a></p>' % (question_id, twitter_src)
 
     result_question+="""
                         %s
                         <p><small>Submitted By: %s - Score: %d - View Count: %d</small></p>
+                        %s %s
                         
                     </section>
-    """ % (fields_of_study, question['submitter'], question['score'], question['view_count'])
+    """ % (fields_of_study, question['submitter'], question['score'], question['view_count'], share_to_fb, share_to_tw)
 
     return result_question
 
--- youask/controller/ctrl_questions.py
+++ youask/controller/ctrl_questions.py
@@ -3,7 +3,7 @@ from cgi import FieldStorage, escape
 
 from model.model_functions import getQuestion
 
-def conventdate(arr):
+def convertDate(arr):
     for i in range(len(arr)):
         arr[i]["submission_date"]=arr[i]["submission_date"].strftime("%y")+ arr[i]["submission_date"].strftime("%m")+arr[i]["submission_date"].strftime("%d")+arr[i]["submission_date"].strftime("%H")+ arr[i]["submission_date"].strftime("%M")+arr[i]["submission_date"].strftime("%S")
         arr[i]["submission_date"]=int(arr[i]["submission_date"])
@@ -32,10 +32,10 @@ def insertionSort(arr,text):
 
 
 def controllerQuestions():
-    facebooksrc = os.getcwd() + "/images/Facebook.png"
-    twittersrc = os.getcwd() + "/images/Twitter.png"
+    facebook_src = os.getcwd() + "images/Facebook.png"
+    twitter_src = os.getcwd() + "images/Twitter.png"
     result = getQuestion()
-    result=conventdate(result)
+    result=convertDate(result)
     form_data = FieldStorage()
     text=""
     if len(form_data) != 0:
@@ -44,10 +44,10 @@ def controllerQuestions():
     result=insertionSort(result,text)
     questions = '<h1>question &nbsp submitter &nbsp score &nbsp view count &nbsp time &nbsp</h1>  '
     for x in result:
-        SharetoFb = '&nbsp &nbsp &nbsp <a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;"> <img src=%s style="border:none 0;" alt="Share to Facebook" /></a> ' % (x, facebooksrc)
-        SharetoTw = '&nbsp &nbsp &nbsp <a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja"><img src=%s style="border:none 0;" alt="Share to Twitter" /></a><p></p>' % (x, twittersrc)
+        share_to_fb = '&nbsp &nbsp &nbsp <a href="https://www.facebook.com/sharer.php?u=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py" target="_blank" ;"> <img src=%s style="border:none 0;" alt="Share to Facebook" /></a> ' % (x, facebook_src)
+        share_to_tw = '&nbsp &nbsp &nbsp <a href="https://twitter.com/share" target="_blank" data-url=https://cs1.ucc.ie/~yc5/cgi-bin/youask/question_pages/question_%s.py data-text="" data-via=""data-lang="ja"><img src=%s style="border:none 0;" alt="Share to Twitter" /></a><p></p>' % (x, twitter_src)
         questions += '<section>%s &nbsp %s &nbsp %s &nbsp %s &nbsp %s %s %s</section> ' % (
-            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"],SharetoFb,SharetoTw)
+            x["question"], x["submitter"], x["score"], x["view_count"], x["submission_date"], share_to_fb, share_to_tw)
 
     return questions
 
