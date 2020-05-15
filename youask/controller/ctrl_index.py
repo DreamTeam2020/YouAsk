403b9082bf4723150e504f97d8ab0c9e0e96b476
YC-git <MY_NAME@example.com>
2020-05-15 12:29:54 +0100

    try to fix messy code

--- youask/controller/ctrl_index.py
+++ youask/controller/ctrl_index.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import json
 import requests
 
 
-def generateNews(n):
+def generateNews(num):
     url = ('http://newsapi.org/v2/top-headlines?'
            'country=us&'
            'apiKey=1e2203fa10234d0a999cf82f685ac8d2')
@@ -10,11 +11,25 @@ def generateNews(n):
     convertdata=response.text
     jsonData = json.loads(convertdata)
     result = ""
-    for x in range(n):
-        result += '<article><h1>Title:%s</h1><h4>%s</h4></article><br>' % (
-        jsonData["articles"][x]["title"].encode(), jsonData["articles"][x]["description"].encode())
+    for x in range(num):
+        result += '<article><h1>Title:%s</h1><p>%s</p></article><br>' % (
+        jsonData["articles"][x]["title"], jsonData["articles"][x]["description"])
     return result
 
+
+def generateNews1(num):
+    url = ('http://newsapi.org/v2/top-headlines?'
+           'country=us&'
+           'apiKey=1e2203fa10234d0a999cf82f685ac8d2')
+    response = requests.get(url)
+    convertdata=response.text.encode('utf-8')
+    jsonData = json.loads(convertdata)
+    result = ""
+    for x in range(num):
+        result += '<article><h1>Title:%s</h1><p>%s</p></article><br>' % (
+        jsonData["articles"][x]["title"], jsonData["articles"][x]["description"])
+ 
+    return result
 
 
 
