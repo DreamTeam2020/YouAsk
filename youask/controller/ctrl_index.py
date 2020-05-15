fcffb2bcb2009e9efaf77348c47ec79a8f4b4019
YC-git <MY_NAME@example.com>
2020-05-15 12:32:06 +0100

    fix an error in ctrl_index.py

--- youask/controller/ctrl_index.py
+++ youask/controller/ctrl_index.py
@@ -3,21 +3,9 @@ import json
 import requests
 
 
+
+
 def generateNews(num):
-    url = ('http://newsapi.org/v2/top-headlines?'
-           'country=us&'
-           'apiKey=1e2203fa10234d0a999cf82f685ac8d2')
-    response = requests.get(url)
-    convertdata=response.text
-    jsonData = json.loads(convertdata)
-    result = ""
-    for x in range(num):
-        result += '<article><h1>Title:%s</h1><p>%s</p></article><br>' % (
-        jsonData["articles"][x]["title"], jsonData["articles"][x]["description"])
-    return result
-
-
-def generateNews1(num):
     url = ('http://newsapi.org/v2/top-headlines?'
            'country=us&'
            'apiKey=1e2203fa10234d0a999cf82f685ac8d2')
@@ -28,7 +16,8 @@ def generateNews1(num):
     for x in range(num):
         result += '<article><h1>Title:%s</h1><p>%s</p></article><br>' % (
         jsonData["articles"][x]["title"], jsonData["articles"][x]["description"])
- 
+    #result.strip("b'")
+
     return result
 
 
