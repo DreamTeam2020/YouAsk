3e657001fa94258e26be086a2debbadecc389fc4
CristianGaudino <gaudino.cristian@gmail.com>
2020-05-15 15:57:05 +0100

    Last method didnt work, trying again with a slight change

--- youask/controller/ctrl_submit.py
+++ youask/controller/ctrl_submit.py
@@ -29,7 +29,7 @@ def controllerSubmission():
         if len(form_data)!=0:
             # Check which heading was selected and then generate the next form using the sub fields
 
-            if not form_data.getfirst('fields_of_study', ''):
+            if not form_data.getlist('fields_of_study'):
                 # This will only occur on the sub checklist, the heading list will never be blank
                 error_msg = '<p class="error">Please Select At Least One Field</p>'
                 question = escape(form_data.getfirst('question', '').strip())
@@ -40,12 +40,11 @@ def controllerSubmission():
                 result = generateQuestionForm(url, question, description, fields, error_msg)
                 removeKeyFromSession(session_table_key, False)
             else:
-                fields_of_study = form_data.getfirst('fields_of_study', '')
+                fields_of_study = form_data.getlist('fields_of_study')
                 if fields_of_study[0] == 'humanities' or fields_of_study[0] == 'natural_sciences' or \
-                                fields_of_study[0] == 'formal_sciences' or fields_of_study[0] == 'professions':
+                        fields_of_study[0] == 'formal_sciences' or fields_of_study[0] == 'professions':
                     # If the data in fields_of_study is equal to one of the main fields
-                    table_name = "ask_%s" % fields_of_study[
-                        0]  # Append fields_of_study to ask and get all fields from that table
+                    table_name = "ask_%s" % fields_of_study[0]  # Append fields_of_study to ask and get all fields from that table
                     fields = getFieldsOfStudy(table_name)  # Get all fields from table_name
 
                     # Get user's current fields from the table
