ee6c82e42a18680591e9674bb8630be9bc967e58
YC-git <MY_NAME@example.com>
2020-07-04 02:41:42 +0100

    change back index.py

--- youask/index.py
+++ youask/index.py
@@ -17,26 +17,18 @@ result = controllerIndex()
 print("""
     %s
     <body>
-<script>
-function myfunction() {
-  setInterval(function(){ alert("Hello"); }, 3000);
-}
-myfunction();
-</script>
-    
         %s
         %s
-        
+
         <div class="container-fluid">
             <div class="row">
                 <aside class="col-3 bg-primary" >you connected friends will displayed here and you can talk with them, now it doesnt work</aside>
-            
-                <main class="col-6 bg-secondary" >   %s  </main>
 
+                <main class="col-6 bg-secondary" >   %s  </main>
                 %s
             </div>
         </div>
-        
+
         %s
     """ % (pageStart("Home", page_name, False), generateHeader(False), generateNav(page_name, False), result,
            generateAsideRight(False), pageEnd()))
